a10da7bc595417de06904a96f48e50867a512ec0
Riad Elimemmedov <riad.elimemmedov@mail.ru>
2022-09-03 08:34:46 +0400

    Django & Redis & Celery & RabbitMq Sending Email

--- django_celery_project/settings.py
+++ django_celery_project/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lk$e80q72@nmh6#v3necp3p89^#u%1vi&fqp6^ds)w06o8-01q'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG',default=True,cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -64,13 +66,13 @@ CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers.DatabaseScheduler' #=> dj
 
 #!Configuration SMPT For Django Sending Email or Gmail
 #Seding Email Used Django SMPT
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'riadalimammedovriad@gmail.com'
-EMAIL_HOST_PASSWORD ='oinkjumckqhxxlxe'
-DEFAULT_FROM_EMAIL = 'Celery <riadalimammedovriad@gmail.com>'
+EMAIL_BACKEND = config('EMAIL_BACKEND')
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT',default=587,cast=int)
+EMAIL_USE_TLS = config('EMAIL_USE_TLS',default=True,cast=bool)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
 
 #!Celery and Redis Command For Starting Server
 #Celery => celery -A django_celery_project.celery worker --pool=solo -l info
